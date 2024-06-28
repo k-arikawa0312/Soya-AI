24fccf55c42351762c2d70b3d12d48c9d0729e88
skmtrd <s1f102400314@iniad.org>
2024-06-29 06:40:55 +0900

    スクレイピング成功

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,8 +1,14 @@
+import { load } from 'cheerio';
+import iconv from 'iconv-lite';
 import { transaction } from 'service/prismaClient';
 
 export const novelUseCase = {
   scrape: (aozoraUrl: string): Promise<string> =>
     transaction('RepeatableRead', async (tx) => {
-      return aozoraUrl;
+      const buffer = await fetch(aozoraUrl).then((b) => b.arrayBuffer());
+      const html = iconv.decode(Buffer.from(buffer), 'Shift_JIS');
+      const $ = load(html);
+      const body = $('body').text();
+      return body;
     }),
 };
