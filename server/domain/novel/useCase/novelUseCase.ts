cd596dd0cedc3be3dff5333cb02bd43ec244e265
skmtrd <s1f102400314@iniad.org>
2024-07-01 00:34:44 +0900

    ss

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -8,7 +8,7 @@ export const novelUseCase = {
       const buffer = await fetch(aozoraUrl).then((b) => b.arrayBuffer());
       const html = iconv.decode(Buffer.from(buffer), 'Shift_JIS');
       const $ = load(html);
-      const body = $('body').text();
+      const body = $('.main_text').text();
       return body;
     }),
 };
